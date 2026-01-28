be87b6834d3ef439ef0552b4d7727e69499a14eb
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2026-01-28 20:11:14 -0300

    Use log-in router

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const { signUpRouter } = require("./sign-up");
+const { logInRouter } = require("./log-in");
 
 const indexRouter = Router();
 
 indexRouter.use("/sign-up", signUpRouter);
+indexRouter.use("/log-in", logInRouter);
 
 module.exports = { indexRouter };
