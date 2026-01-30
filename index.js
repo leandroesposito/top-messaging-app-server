5cff67bb62244d839c77ad8ac4c6a30c20647a51
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2026-01-29 23:34:01 -0300

    Use passport with jwt strategy

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const { indexRouter } = require("./routes");
+const passport = require("passport");
+const jwtStratety = require("./auth/jwt-strategy");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+passport.use(jwtStratety);
 
 app.use("/", indexRouter);
 
