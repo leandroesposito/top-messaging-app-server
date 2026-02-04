7fabe161b6cb80f8b7db650fc4609d4f6c098d37
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2026-02-04 20:54:17 -0300

    Use refresh router

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const { signUpRouter } = require("./sign-up");
 const { logInRouter } = require("./log-in");
+const { refreshRouter } = require("./refresh");
 
 const indexRouter = Router();
 
 indexRouter.use("/sign-up", signUpRouter);
 indexRouter.use("/log-in", logInRouter);
+indexRouter.use("/refresh", refreshRouter);
 
 module.exports = { indexRouter };
