ed8db9dcf6dc4b50a4e5bbcde57d7e1d2d2f0b29
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2026-02-05 18:34:58 -0300

    Fix add missing await in database operation

--- controllers/sign-up.js
+++ controllers/sign-up.js
@@ -41,7 +41,7 @@ const signUp = [
     const { username, password } = req.body;
     const securePassword = await bcrypt.hash(password, 10);
 
-    const newUserId = userDB.createUser(username, securePassword);
+    const newUserId = await userDB.createUser(username, securePassword);
 
     if (newUserId) {
       res.status(200).json({ message: "User created succesfuly" });
