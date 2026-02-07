f86022349abc1ffa7b3b409d20d96a713963372f
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2026-02-07 20:22:53 -0300

    Add json response on failed authentication

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -1,7 +1,25 @@
 const passport = require("passport");
 
-function authenticate() {
-  return passport.authenticate("jwt", { session: false });
+function authenticate(req, res, next) {
+  passport.authenticate("jwt", { session: false }, function (err, user, info) {
+    if (err) {
+      return next(err);
+    }
+
+    if (!user) {
+      let error = "invalid token";
+      if (info && info.name) {
+        if (info.name == "TokenExpiredError") {
+          error =
+            "Access token expired, use refreshToken in /api/auth/refresh or log in to get new access token.";
+        }
+      }
+      return res.status(401).json({ errors: [error] });
+    }
+
+    req.user = user;
+    next();
+  })(req, res, next);
 }
 
 module.exports = { authenticate };
