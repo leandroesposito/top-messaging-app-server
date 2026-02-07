bbd1d53782cc0181294843a741d301ee48e819b0
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2026-02-07 16:33:43 -0300

    Use log-out router

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ const { Router } = require("express");
 const { signUpRouter } = require("./sign-up");
 const { logInRouter } = require("./log-in");
 const { refreshRouter } = require("./refresh");
+const { logOutRouter } = require("./log-out");
 
 const indexRouter = Router();
 
 indexRouter.use("/sign-up", signUpRouter);
 indexRouter.use("/log-in", logInRouter);
 indexRouter.use("/refresh", refreshRouter);
+indexRouter.use("/log-out", logOutRouter);
 
 module.exports = { indexRouter };
