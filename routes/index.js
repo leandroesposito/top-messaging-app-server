530ce9efdb006daa5f3f20118d117f96682a09dc
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2026-02-08 21:31:04 -0300

    Use user router

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const { authRouter } = require("./auth");
+const { userRouter } = require("./user");
 
 const indexRouter = Router();
 
 indexRouter.use("/auth", authRouter);
+indexRouter.use("/users", userRouter);
 
 module.exports = { indexRouter };
