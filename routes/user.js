3dc294e3b0eaad4f7d7fa814e39e5615e30d690c
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2026-02-11 00:17:00 -0300

    Use friends router

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 const profileController = require("../controllers/profile");
+const { friendsRouter } = require("./friends");
 
 const userRouter = Router();
 
 userRouter.put("/profile", profileController.modifyProfile);
 userRouter.get("/:userId/profile", profileController.getProfile);
+userRouter.use("/friends", friendsRouter);
 
 module.exports = { userRouter };
