e398c51555004c21e18c42c047ec8fb62a53b0c7
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2026-02-11 21:13:59 -0300

    Add delete route

--- routes/friends.js
+++ routes/friends.js
@@ -5,5 +5,6 @@ const friendsRouter = Router();
 
 friendsRouter.get("/", friendsController.getFriends);
 friendsRouter.post("/:friendCode", friendsController.addFriend);
+friendsRouter.delete("/:userId", friendsController.deleteFriend);
 
 module.exports = { friendsRouter };
