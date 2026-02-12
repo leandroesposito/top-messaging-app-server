5649eb2f77cd925f5f01e8fd3b5d286977b1d040
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2026-02-11 21:13:24 -0300

    Add deleteFriendsPair

--- db/friends.js
+++ db/friends.js
@@ -10,6 +10,17 @@ async function addFriendsPair(uid1, uid2) {
   return true;
 }
 
+async function deleteFriendsPair(uid1, uid2) {
+  const [lower, higher] =
+    parseInt(uid1) < parseInt(uid2) ? [uid1, uid2] : [uid2, uid1];
+
+  const query = `DELETE FROM friends WHERE uid1 = $1 AND uid2 = $2`;
+  const params = [lower, higher];
+
+  await runQuery(query, params);
+  return true;
+}
+
 async function friendsPairExist(uid1, uid2) {
   const [lower, higher] = uid1 < uid2 ? [uid1, uid2] : [uid2, uid1];
 
@@ -28,4 +39,5 @@ async function friendsPairExist(uid1, uid2) {
 module.exports = {
   addFriendsPair,
   friendsPairExist,
+  deleteFriendsPair,
 };
