a41bb939e30e54923e621d67dc9197b0a1dfef24
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2026-02-12 19:13:36 -0300

    Use group router

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const { authRouter } = require("./auth");
 const { userRouter } = require("./user");
+const { groupRouter } = require("./group");
 
 const indexRouter = Router();
 
 indexRouter.use("/auth", authRouter);
 indexRouter.use("/users", userRouter);
+indexRouter.use("/groups", groupRouter);
 
 module.exports = { indexRouter };
